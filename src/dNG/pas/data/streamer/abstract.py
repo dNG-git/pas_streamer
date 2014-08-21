2d7fd51e537f084f5ea7d05f59d5d87ecbb985af
NotTheEvilOne <git-NTEO@vplace.de>
2014-08-21 20:26:05 +0200

    Trivial: Removed file extension from URLs in the file header

--- src/dNG/pas/data/streamer/abstract.py
+++ src/dNG/pas/data/streamer/abstract.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;streamer
+https://www.direct-netware.de/redirect?pas;streamer
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasStreamerVersion)#
 #echo(__FILEPATH__)#
@@ -35,7 +35,7 @@ interface is similar to a file one.
 :package:    pas
 :subpackage: streamer
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
--- src/dNG/pas/data/streamer/abstract_encapsulated.py
+++ src/dNG/pas/data/streamer/abstract_encapsulated.py
@@ -6,13 +6,13 @@ direct PAS
 Python Application Services
 ----------------------------------------------------------------------------
 (C) direct Netware Group - All rights reserved
-http://www.direct-netware.de/redirect.py?pas;streamer
+https://www.direct-netware.de/redirect?pas;streamer
 
 This Source Code Form is subject to the terms of the Mozilla Public License,
 v. 2.0. If a copy of the MPL was not distributed with this file, You can
 obtain one at http://mozilla.org/MPL/2.0/.
 ----------------------------------------------------------------------------
-http://www.direct-netware.de/redirect.py?licenses;mpl2
+https://www.direct-netware.de/redirect?licenses;mpl2
 ----------------------------------------------------------------------------
 #echo(pasStreamerVersion)#
 #echo(__FILEPATH__)#
@@ -32,7 +32,7 @@ The abstract streamer encapsulates another streamer for transforming it.
 :package:    pas
 :subpackage: streamer
 :since:      v0.1.00
-:license:    http://www.direct-netware.de/redirect.py?licenses;mpl2
+:license:    https://www.direct-netware.de/redirect?licenses;mpl2
              Mozilla Public License, v. 2.0
 	"""
 
